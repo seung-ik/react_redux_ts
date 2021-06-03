2d7abe3e15b2c053e93b2d2177dd710c4a81b9f6
seung-ik <69374857+seung-ik@users.noreply.github.com>
2021-06-03 19:28:24 +0900

    file organizing

---
+++ src/state/actions/index.ts
@@ -0,0 +1,13 @@
+interface DepositAction {
+  type: "deposit",
+  payload: number
+}
+interface WithdrawAction {
+  type: "withdraw",
+  payload: number
+}
+interface BankruptAction{
+  type:'bankrupt'
+}
+
+export type Action = DepositAction | WithdrawAction | BankruptAction
--- src/state/reducers/bankReducer.ts
+++ src/state/reducers/bankReducer.ts
@@ -1,26 +1,10 @@
 // 리듀서는 뭐하는 거냐면은 음 store 안에서 state 를 액션을 확인하고 state 를 newstate 로 바꾸는 함수야 
-
-import { BreakOrContinueStatement } from "typescript";
-
 // 액션의 형식 은 type 과 payload >> reducer에서는 탕ㅂ을 확인한 이후 payload 의 값을 이용해 state 값에 변화를 준다.
-
-
+import {Action} from "../actions"
 
 const initialState = 0 ;
 
-interface DepositAction {
-  type: "deposit",
-  payload: number
-}
-interface WithdrawAction {
-  type: "withdraw",
-  payload: number
-}
-interface BankruptAction{
-  type:'bankrupt'
-}
-type Action = DepositAction | WithdrawAction | BankruptAction
-const reducer = (state: number = initialState, action:Action)=>{
+const reducer = (state: number = initialState, action:Action) : number=>{
   switch(action.type){
     case "deposit":
       return state + action.payload;
